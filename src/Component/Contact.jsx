3fc0803ecdb4865e954a0a7afdf0f888537fd508
ArlyP <fikriarp11@gmail.com>
2024-07-25 15:55:40 +0700

    add : update

--- src/Component/Contact.jsx
+++ src/Component/Contact.jsx
@@ -6,7 +6,7 @@ export default function Contact() {
         Contact me for questions, suggestions, or collaboration through the
         email below. I am ready to assist you
       </p>
-      <div className=" text-white py-5 bg-slate-800 text-lg flex flex-col mt-20 pl-3 shadow-lg shadow-black  gap-4 font-mono">
+      <div className=" text-white py-5 bg-slate-800 text-lg flex flex-col mt-20 pl-3 shadow-lg shadow-black gap-4 font-mono">
         <div className="flex justify-end gap-2 px-4 w-full">
           <div className="w-2 h-2 rounded-full bg-green-600"></div>
           <div className="w-2 h-2 rounded-full bg-red-600"></div>
@@ -15,7 +15,7 @@ export default function Contact() {
         <a
           href="https://wa.me/6282361611007"
           target="_black"
-          className="text-lg flex items-center gap-2 hover:drop-shadow-lg cursor-pointer hover:-translate-y-1"
+          className="text-sm flex items-center gap-2 hover:drop-shadow-lg cursor-pointer hover:-translate-y-1"
         >
           <i className="fa-brands fa-whatsapp text-green-600"></i>
           <p>+62 823 6161 1007</p>
@@ -23,7 +23,7 @@ export default function Contact() {
         <a
           href="https://www.instagram.com/fikriarly/"
           target="_black"
-          className="text-lg flex items-center gap-2 hover:drop-shadow-lg cursor-pointer hover:-translate-y-1"
+          className="text-sm flex items-center gap-2 hover:drop-shadow-lg cursor-pointer hover:-translate-y-1"
         >
           <i className="fa-brands fa-instagram text-red-500"></i>
           <p>@fikriarly</p>
@@ -31,7 +31,7 @@ export default function Contact() {
         <a
           href="mailto:fikriarly11@gmail.com"
           target="_black"
-          className="text-lg flex items-center gap-2 hover:drop-shadow-lg cursor-pointer hover:-translate-y-1"
+          className="text-sm flex items-center gap-2 hover:drop-shadow-lg cursor-pointer hover:-translate-y-1"
         >
           <i className="fa-regular fa-envelope text-red-700"></i>
           <p>fikriarly11@gmail.com</p>
